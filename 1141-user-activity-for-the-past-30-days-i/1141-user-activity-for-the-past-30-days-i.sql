87839c6dcaae57a13c50997779322e70daa403cc
SayalKullarwar <sayalkullarwar26@gmail.com>
2026-08-03 21:35:17 +0530

    Time: 589 ms (19.69%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -4,6 +4,5 @@ activity_date as day,
 COUNT(DISTINCT user_id) AS active_users
 FROM Activity 
 WHERE activity_date 
-BETWEEN '2019-06-28' 
-AND '2019-07-27'
+BETWEEN '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date;
