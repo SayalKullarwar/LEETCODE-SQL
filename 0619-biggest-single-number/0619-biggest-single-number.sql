d299fbbeb168ae2290c5d5677778254cfb22da09
SayalKullarwar <sayalkullarwar26@gmail.com>
2026-08-04 22:23:06 +0530

    Time: 520 ms (18.44%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-SELECT 
-    MAX(NUM)AS num
-FROM (SELECT 
-            num
+SELECT MAX(NUM)AS num
+FROM (SELECT num
         FROM MyNumbers
         GROUP BY num
         HAVING COUNT(num) = 1
